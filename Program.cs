8b3968a595e264f10ad788c3bed0ce7496c3e329
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2020-10-22 19:54:05 +0100

    debug messages

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.Threading;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
@@ -27,32 +26,38 @@ namespace WaitForStatusCheckAction
 
         private static async Task WaitForStatusCheckAction(Context context)
         {
-            Console.WriteLine($"StatusCheckName: {context.StatusChecks.AsCsv()}");
-            Console.WriteLine($"Repository: {context.Repository}");
-            Console.WriteLine($"Sha: {context.Sha}");
-            Console.WriteLine($"Check Interval: {context.WaitInterval}");
-            Console.WriteLine($"Timeout: {context.Timeout}");
-            
-            Task.WaitAll(new[]{CheckCommitStatus(context)}, (int)context.Timeout.TotalMilliseconds);
+            Console.WriteLine($"::debug::StatusCheckName: {context.StatusChecks.AsCsv()}");
+            Console.WriteLine($"::debug::Repository: {context.Repository}");
+            Console.WriteLine($"::debug::Sha: {context.Sha}");
+            Console.WriteLine($"::debug::Check Interval: {context.WaitInterval.TotalSeconds}s");
+            Console.WriteLine($"::debug::Timeout: {context.Timeout.TotalSeconds}s");
+
+            Task.WaitAll(new[] { CheckCommitStatus(context) }, (int)context.Timeout.TotalMilliseconds);
             await Task.CompletedTask;
         }
 
         private static async Task CheckCommitStatus(Context context)
         {
             var statuses = await apiClient.GetFromJsonAsync<Status[]>($"/repos/{context.Repository}/commits/{context.Sha}/statuses");
-            if (!statuses.Any())
+            var requiredStatus = statuses.Where(s => context.StatusChecks.Contains(s.Context, StringComparer.InvariantCultureIgnoreCase));
+            if (!requiredStatus.Any())
             {
-                Console.WriteLine($"No commit status found in repo for the given sha.");
+                Console.WriteLine($"::debug::No commit status matching given conditions found in repo for the given sha.");
                 Console.WriteLine($"Waiting for {context.WaitInterval.TotalSeconds}s before next check");
                 await Task.Delay(context.WaitInterval);
                 await CheckCommitStatus(context);
                 return;
             }
-            foreach (var status in statuses)
+
+            var statusChecksStates = new bool[context.StatusChecks.Length];
+            var done = requiredStatus.All(status =>
             {
                 Console.WriteLine($"{status.Context} is at state {status.State}");
-            }
-            await Task.CompletedTask;
+                return status.State.Equals("success");
+            });
+
+            var returnTask = done ? Task.CompletedTask : CheckCommitStatus(context);
+            await returnTask;
         }
 
         private static void InitialiseApiClient(Context context)
