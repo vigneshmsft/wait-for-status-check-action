e7a4036acaff3c0fb818671b1af2ad0a163192bf
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2020-10-22 15:04:15 +0100

    NullIfEmpty

--- Program.cs
+++ Program.cs
@@ -10,7 +10,23 @@ namespace WaitForStatusCheckAction
             {
                 Console.WriteLine("Hello {0}", arg);
             }
+            var statusCheckName = args[0];
+            var repository = NullIfEmpty(args[1]) ?? Environment.GetEnvironmentVariable("GITHUB_REPOSITORY");
+            var sha = NullIfEmpty(args[2]) ?? Environment.GetEnvironmentVariable("GITHUB_SHA");
+            
+            WaitForStatusCheckAction(statusCheckName, repository, sha);
             Console.WriteLine($"::set-output name=time::{DateTime.Now}");
+        }
+
+        private static void WaitForStatusCheckAction(string statusCheckName, string repository, string commitSha)
+        {
+            Console.WriteLine($"{nameof(statusCheckName)}: {statusCheckName}");
+            Console.WriteLine($"{nameof(repository)}: {repository}");
+            Console.WriteLine($"{nameof(commitSha)}: {commitSha}");
+        }
+
+        private static string NullIfEmpty(string inputString){
+            return string.IsNullOrWhiteSpace(inputString) ? null : inputString;
         }
     }
 }
